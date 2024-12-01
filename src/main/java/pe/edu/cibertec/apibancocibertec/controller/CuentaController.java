b5a76fee88f3194d50ecff2fb93413d053b0e057
Grupo5CicloWeb <cicloc504@gmail.com>
2024-12-01 10:57:08 -0500

    habilitando cors

--- src/main/java/pe/edu/cibertec/apibancocibertec/controller/CuentaController.java
+++ src/main/java/pe/edu/cibertec/apibancocibertec/controller/CuentaController.java
@@ -4,16 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import pe.edu.cibertec.apibancocibertec.dto.CuentaDto;
 import pe.edu.cibertec.apibancocibertec.dto.TransferenciaDto;
 import pe.edu.cibertec.apibancocibertec.service.ICuentaService;
 
 import java.util.List;
-
+/*@CrossOrigin(origins = {"http://frontbancociber.com"},
+methods = {RequestMethod.GET, RequestMethod.POST})*/
 @RequiredArgsConstructor
 @Controller
 @RequestMapping("/api/v1/cuenta")
