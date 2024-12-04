2040ef0012a7d0a16ab8628b3a6ed955adc7ba57
Grupo5CicloWeb <cicloc504@gmail.com>
2024-12-04 10:30:21 -0500

    Filtro JWT terminado

--- src/main/java/pe/edu/cibertec/apibancocibertec/security/FiltroJWTAuthorization.java
+++ src/main/java/pe/edu/cibertec/apibancocibertec/security/FiltroJWTAuthorization.java
@@ -1,24 +1,54 @@
 package pe.edu.cibertec.apibancocibertec.security;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.*;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class FiltroJWTAuthorization extends OncePerRequestFilter {
     private final String CLAVE ="@Cibertec2024";
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-
+        try{
+            if(utilizaToken(request)){
+                Claims claims = validateToken(request);
+                if(claims.get("authorities") != null){
+                    cargarAutorizaciones(claims);
+                }else{
+                    SecurityContextHolder.clearContext();
+                }
+            }else{
+                SecurityContextHolder.clearContext();
+            }
+            filterChain.doFilter(request, response);
+        }catch (ExpiredJwtException | UnsupportedJwtException | MalformedJwtException ex){
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            ((HttpServletResponse) response).sendError(HttpServletResponse.SC_FORBIDDEN, ex.getMessage());
+        }
     }
 
-
+    private void cargarAutorizaciones(Claims claims){
+        List<String> autorizaciones = (List<String>) claims.get("autorizaciones");
+        UsernamePasswordAuthenticationToken authenticationToken
+                = new UsernamePasswordAuthenticationToken(
+                        claims.getSubject(),
+                null,
+                autorizaciones.stream().map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList())
+        );
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+    }
 
     private Claims validateToken(HttpServletRequest request) {
         String token = request.getHeader("Authorization")
