6dfcdfe7e3343cd8b479886e0d919e1f174f5c29
Grupo5CicloWeb <cicloc504@gmail.com>
2024-12-04 09:37:29 -0500

    spring security

--- src/main/java/pe/edu/cibertec/apibancocibertec/service/UsuarioService.java
+++ src/main/java/pe/edu/cibertec/apibancocibertec/service/UsuarioService.java
@@ -1,4 +1,18 @@
 package pe.edu.cibertec.apibancocibertec.service;
 
-public class UsuarioService {
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import pe.edu.cibertec.apibancocibertec.model.Usuario;
+import pe.edu.cibertec.apibancocibertec.repository.UsuarioRepository;
+
+@RequiredArgsConstructor
+@Service
+public class UsuarioService implements IUsuarioService  {
+
+    private final UsuarioRepository usuarioRepository;
+
+    @Override
+    public Usuario obtenerUsuarioxNomusuario(String nomusuario) {
+        return usuarioRepository.findByNomusuario(nomusuario);
+    }
 }
