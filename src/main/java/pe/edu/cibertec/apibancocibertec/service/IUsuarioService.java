31f0aade50bf7e29bc68b7c7abcbb3b5533ea728
Grupo5CicloWeb <cicloc504@gmail.com>
2024-12-04 09:36:24 -0500

    spring security

--- src/main/java/pe/edu/cibertec/apibancocibertec/service/IUsuarioService.java
+++ src/main/java/pe/edu/cibertec/apibancocibertec/service/IUsuarioService.java
@@ -1,4 +1,9 @@
 package pe.edu.cibertec.apibancocibertec.service;
 
+import pe.edu.cibertec.apibancocibertec.model.Usuario;
+
 public interface IUsuarioService {
+
+    Usuario obtenerUsuarioxNomusuario(String nomusuario);
+
 }
